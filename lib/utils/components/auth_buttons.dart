5681f04a7954155d81faf296e0cbdf3b175955a6
mehsen-alk <mehsen2222@gmail.com>
2023-07-05 23:12:01 +0300

    remove register button

--- lib/utils/components/auth_buttons.dart
+++ lib/utils/components/auth_buttons.dart
@@ -7,7 +7,8 @@ class AuthButtons extends StatelessWidget {
   final VoidCallback secondButtonTab;
   final bool? loading;
   const AuthButtons(
-      {super.key, required this.firstButtonTitle,
+      {super.key,
+      required this.firstButtonTitle,
       required this.secondButtonTitle,
       this.firstButtonTab,
       required this.secondButtonTab,
@@ -56,39 +57,39 @@ class AuthButtons extends StatelessWidget {
                   )),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-                right: 16.0, left: 16, bottom: 8.0, top: 8.0),
-            child: SizedBox(
-              width: double.maxFinite,
-              height: 50,
-              child: OutlinedButton(
-                  onPressed: secondButtonTab,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateColor.resolveWith(
-                        (states) => const Color.fromARGB(111, 255, 255, 255)),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                    ),
-                    side: MaterialStateProperty.all(
-                      const BorderSide(
-                          color: Color(0xff03816A),
-                          width: 3,
-                          style: BorderStyle.solid),
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      secondButtonTitle,
-                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                            color: const Color(0xff03816A),
-                          ),
-                    ),
-                  )),
-            ),
-          ),
+          // Padding(
+          //   padding: const EdgeInsets.only(
+          //       right: 16.0, left: 16, bottom: 8.0, top: 8.0),
+          //   child: SizedBox(
+          //     width: double.maxFinite,
+          //     height: 50,
+          //     child: OutlinedButton(
+          //         onPressed: secondButtonTab,
+          //         style: ButtonStyle(
+          //           backgroundColor: MaterialStateColor.resolveWith(
+          //               (states) => const Color.fromARGB(111, 255, 255, 255)),
+          //           shape: MaterialStateProperty.all(
+          //             RoundedRectangleBorder(
+          //               borderRadius: BorderRadius.circular(40),
+          //             ),
+          //           ),
+          //           side: MaterialStateProperty.all(
+          //             const BorderSide(
+          //                 color: Color(0xff03816A),
+          //                 width: 3,
+          //                 style: BorderStyle.solid),
+          //           ),
+          //         ),
+          //         child: Center(
+          //           child: Text(
+          //             secondButtonTitle,
+          //             style: Theme.of(context).textTheme.labelLarge?.copyWith(
+          //                   color: const Color(0xff03816A),
+          //                 ),
+          //           ),
+          //         )),
+          //   ),
+          // ),
         ],
       ),
     );
